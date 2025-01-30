5a1ce72017e90e522adb777747c63352f0fb5547
MariaXiaEgedal <mariaegedal@gmail.com>
2025-01-30 21:34:14 +0100

    laver påproduct_list

---
+++ statisk_site/js/product.js
@@ -0,0 +1,7 @@
+let products = document.querySelector(".productContainer")
+fetch("https://kea-alt-del.dk/t7/api/products/665")
+    .then(response => response.json())
+    
+    .then(data => {
+
+}) 
